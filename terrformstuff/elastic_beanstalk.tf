58f00d090cdb2dc521b4c42ca531abb5527a90cd
CS9490 <cristianstatescu.cs9490@gmail.com>
2023-11-25 23:33:42 -0500

    terraform apply successfully ran

---
+++ terrformstuff/elastic_beanstalk.tf
@@ -0,0 +1,15 @@
+resource "aws_elastic_beanstalk_application" "application" {
+    name = "nawayfarer"  
+}
+
+resource "aws_elastic_beanstalk_environment" "environment" {
+  name                = "nawayfarer-environment"
+  cname_prefix        = "cs9490nawayfarer"
+  application         = aws_elastic_beanstalk_application.application.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.6 running Python 3.11"
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+}
